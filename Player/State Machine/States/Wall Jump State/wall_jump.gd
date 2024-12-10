b0366b82eb2f7f56d1939d1045f78dc5b8737bd7
LamCaVoi <nguyenngocthanhlamk16@siu.edu.vn>
2024-12-10 10:25:22 +0700

    commit

--- Player/State Machine/States/Wall Jump State/wall_jump.gd
+++ Player/State Machine/States/Wall Jump State/wall_jump.gd
@@ -3,21 +3,36 @@ extends Player_State
 var wall_jump_gravity:float
 var input_block_timer: float
 var jump_dir: float
+var max_air_speed: float
+
+func handle_input(event: InputEvent) -> void:
+	if movement_input.wants_jump():
+		wall_dir = movement.find_wall(4)
+		if(wall_dir):
+			finished.emit("Wall Jump")
+	elif movement_input.wants_climb():
+		wall_dir = movement.find_wall(1)
+		if(wall_dir):
+			finished.emit("Climb")
+	elif movement_input.wants_dash() and can_dash:
+		finished.emit("Dash")
 
 func physics_update(delta: float) -> void:
+	print(parent.velocity)
 	input_block_timer -=delta
-	if(input_block_timer < 0):
-		finished.emit("Fall")
-		return
 	var dir = movement_input.get_horizontal_input_pressed()
+	if(input_block_timer > 0):
+		dir = jump_dir
 	run(dir)
-	if(dir != jump_dir):
-		parent.velocity.y += wall_jump_gravity * delta
-		parent.velocity.x = 75
-	else:
+	if(dir == jump_dir):
 		apply_gravity(delta)
+		if(input_block_timer > 0):
+			parent.velocity.x = lerp(parent.velocity.x, dir * max_air_speed, movement_data.velocity_x_lerp_speed)
+		else:
+			parent.velocity.x = lerp(parent.velocity.x, dir * (max_air_speed + 20), movement_data.velocity_x_lerp_speed)
+	else:
+		parent.velocity.y = clamp(parent.velocity.y + wall_jump_gravity * delta,0,movement_data.max_y_speed)
 		parent.velocity.x = lerp(parent.velocity.x, dir * movement_data.max_air_x_speed, movement_data.velocity_x_lerp_speed)
-	
 	movement.move_x(parent.velocity.x * delta, true)
 	movement.move_y(parent.velocity.y * delta, true)
 	if (is_colliding_y == -1):
@@ -30,18 +45,15 @@ func switch_case(dir):
 			finished.emit("Run")
 		else:
 			finished.emit("Idle")
-	elif movement_input.wants_jump():
-		wall_dir = movement.find_wall()
-		if(wall_dir):
-			finished.emit("Wall Jump")
-	elif movement_input.wants_climb():
-		wall_dir = movement.find_wall()
-		if(wall_dir):
-			finished.emit("Climb")
+	else:
+		wall_dir = movement.find_wall(1)
+		if(wall_dir and wall_dir == dir):
+			finished.emit("Wall Slide")
 
 func enter(previous_state_path: String, data := {}) -> void:
 	wall_jump_gravity = movement_data.jump_gravity * 1.5
 	jump_dir = wall_dir * -1
-	parent.velocity.x = movement_data.max_air_x_speed * jump_dir
-	parent.velocity.y = movement_data.high_jump_velocity
-	input_block_timer = 0.2
+	max_air_speed = movement_data.max_air_x_speed * 1.3
+	parent.velocity.x = 100 * jump_dir
+	parent.velocity.y = movement_data.high_jump_velocity * 0.8
+	input_block_timer = 0.15
