a90a62c94367473dcfe5008eee25103553ac0f25
LamCaVoi <nguyenngocthanhlamk16@siu.edu.vn>
2024-12-25 11:37:25 +0700

    Update room_area.gd

--- room_area.gd
+++ room_area.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-
-func _on_body_entered(body: Node2D) -> void:
-	var rect : Rect2= collision_shape_2d.shape.get_rect()
-	rect.position = to_global(rect.position)
-	Events.player_enter_room.emit(rect)
+func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	var local_shape_owner = shape_find_owner(local_shape_index)
+	var local_shape_node: CollisionShape2D = shape_owner_get_owner(local_shape_owner)
+	var rect = local_shape_node.shape.get_rect()
+	rect.position = local_shape_node.global_position
+	print(rect.position)
+	
+	Events.emit_signal("player_enter_room",rect)
+	
